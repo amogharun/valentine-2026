892364cc94e881b388867357b49495d840d0782a
amogharun <amogharun@gmail.com>
2026-01-30 11:51:32 -0500

    Update config.js

--- config.js
+++ config.js
@@ -81,7 +81,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/degmxycnm/video/upload/v1769791734/Bad_Bunny_ft._Bomba_Este%CC%81reo_-_Ojitos_Lindos_360_Visualizer_Un_Verano_Sin_Ti_qn7spk.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
